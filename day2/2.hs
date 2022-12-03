c08d9caa14e88ec6124d8d65e8ba80189e040ba9
nkdem <github.ratbite@aleeas.com>
2022-12-03 00:18:04 +0000

    Complete Day2 in Haskell

--- day2/2.hs
+++ day2/2.hs
@@ -1,15 +1,17 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Main where
 import Data.Functor
+import Data.List ((\\))
 
 data Move  = Rock
             | Paper
             | Scissors
-    deriving (Show,Eq)
+    deriving (Show,Eq,Enum)
 
 data Status = Win
             | Lose
             | Draw
+    deriving (Show,Eq)
 
 scoreMove :: Move -> Int
 scoreMove Rock = 1
@@ -34,6 +36,16 @@ parseMove 'Y' = Paper
 parseMove 'C' = Scissors
 parseMove 'Z' = Scissors
 
+decodeMove :: Move -> Status
+decodeMove Rock = Lose
+decodeMove Paper = Draw
+decodeMove Scissors = Win
+
+determineMove :: Move -> Status -> Move
+determineMove opponentMove Draw = opponentMove
+determineMove opponentMove Win = winsAgainst opponentMove
+determineMove opponentMove Lose = head $ [Rock ..] \\ [opponentMove, winsAgainst opponentMove]
+
 parseMoves :: String -> [Move]
 parseMoves (x:_:y:_) = map parseMove [x,y]
 
@@ -49,8 +61,17 @@ readInput = readFile "./inputs/2" <&> lines <&> map parseMoves
 
 solve1 :: [[Move]] -> Int
 solve1 [] = 0
-solve1 (x:xs) = sum $ scoreStatus status + scoreMove (x !! 1) : [solve1 xs]
+solve1 (x:xs) = sum $ scoreStatus status + scoreMove myMove : [solve1 xs]
     where status = decideStatus x
+          myMove :: Move
+          myMove = x !! 1
+
+solve2 :: [[Move]] -> Int
+solve2 = solve1 . map (\x -> [head x, determineMove (head x) (decoded $ myMove x) ])
+    where myMove x = x !! 1
+          decoded myMove = decodeMove myMove
 
 main :: IO ()
-main = readInput >>= print . solve1
+main = do
+    readInput >>= print . solve1
+    readInput >>= print . solve2
