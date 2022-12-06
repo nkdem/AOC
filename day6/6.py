ae825d4cb75262982bae2d20a9427f0d8b6a0fcb
nkdem <github.ratbite@aleeas.com>
2022-12-06 08:04:35 +0000

    Refactor

--- day6/6.py
+++ day6/6.py
@@ -1,35 +1,21 @@
 def read_input():
     str = []
     with open('day6/inputs/6') as f:
-        while True:
-            c = f.read(1)
-            if not c:
-                return str
+        while (c := f.read(1)):
             str.append(c)
+        return str
 
 def valid(str):
     return len(set(str)) == len(str)
 
-def solve1(str):
-    tracker = [] * 4
-    for (i,c) in enumerate(str):
-        tracker += c 
-        if (l := len(tracker) == 4) and not (v := valid(tracker)):
-            tracker.pop(0)
-        elif l and v:
-            return i + 1
-def solve2(str):
-    tracker = [] * 4
-    for (i,c) in enumerate(str):
-        tracker += c 
-        if (l := len(tracker) == 14) and not (v := valid(tracker)):
-            tracker.pop(0)
-        elif l and v:
-            return i + 1
+def solve(str, length):
+    for i in range(0, len(str)):
+        if len(set(str[i:length + i])) == length:
+            return i + length
             
         
 def main():
     input = read_input()
-    print(solve1(input))
-    print(solve2(input))
+    print(solve(input,4))
+    print(solve(input,14))
 main()
