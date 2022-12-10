88f1990871df5d20eb1b8522acdfb94627dffaf4
nkdem <github.ratbite@aleeas.com>
2022-12-10 01:36:26 +0000

    Refactor task 1

--- day9/main.py
+++ day9/main.py
@@ -9,39 +9,25 @@ def read_input():
 def adjacent(head,tail):
     return abs(head[X] - tail[X]) <= 1 and abs(head[Y] - tail[Y]) <= 1
 
-def up(cords):
-    cords[Y] += 1
-def right(cords):
-    cords[X] += 1
-def down(cords):
-    cords[Y] -= 1
-def left(cords):
-    cords[X] -= 1
+def move(cords, pos):
+    match pos:
+        case 'U': cords[Y] += 1
+        case 'R': cords[X] += 1
+        case 'D': cords[Y] -= 1
+        case 'L': cords[X] -= 1
 def solve1(instrs):
     head = [0,0]
     tail = [0,0]
     visited = {(0,0): True}
     for (pos, steps) in instrs:
-        initial = head
         while (steps > 0):
             initial = head.copy()
-            match pos:
-                case 'U':
-                    up(head)
-                case 'R':
-                    right(head)
-                case 'D':
-                    down(head)
-                case 'L':
-                    left(head)
+            move(head,pos)
             steps -= 1
             if not adjacent(head,tail):
                 tail = initial
             visited[(tail[X], tail[Y])] = True
     print(len(visited))
-            
-
-                
 
 def main():
     solve1(read_input())
