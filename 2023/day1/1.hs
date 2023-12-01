b9ebf56eded88bb244845f5887c3340c4d3136e5
nkdem <dev@nkdem.net>
2023-12-01 11:00:55 +0000

    Complete day 1a

--- 2023/day1/1.hs
+++ 2023/day1/1.hs
@@ -1,14 +1,17 @@
 import Data.Functor ( (<&>) )
+import Data.Char (isDigit, digitToInt)
+import Data.Bifunctor (Bifunctor(bimap))
 
 -- readInput :: IO 
 readInput = readFile "./inputs/1" <&> lines
 
-solve1 :: [String] -> String
-solve1 = undefined
+solve1 :: [String] -> Integer
+-- bimap head head <=>  (\ (x, y) -> (head x, head y))
+solve1 = sum . map ((\x -> read (fst x : [snd x]) :: Integer) . (\x -> bimap head last (x,x))) . uncurry ( zipWith (++)) . bimap head head . splitAt 1 . replicate 2 . map (filter isDigit)
 
 
 main :: IO ()
-main = do 
+main = do
         readInput >>= print . solve1
         -- equivaelnt to 
         -- input <- readInput
