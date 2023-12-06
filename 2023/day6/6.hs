41dfdb10c76a9abe9b7aef2d35932737b40d386d
nkdem <dev@nkdem.net>
2023-12-06 09:47:29 +0000

    Complete day 6 part b

--- 2023/day6/6.hs
+++ 2023/day6/6.hs
@@ -3,18 +3,26 @@ type Distance = Int
 type Speed = Int
 type RaceDetails = (Time, Distance)
 
-parse :: FilePath -> IO [RaceDetails]
-parse input = do
+parse1 :: FilePath -> IO [RaceDetails]
+parse1 input = do
     (x:xs) <- map (map (\y -> read y :: Int) . tail . words) . lines <$> readFile input
     return $ zip x (head xs)
+
+parse2 :: FilePath -> IO RaceDetails 
+parse2 input = do 
+    (x:xs) <- map ((\x -> read x:: Int) . concat . tail . words) . lines <$> readFile input
+    return (x, head xs)
+    
 
 calcDistance :: Speed -> Time -> Distance
 calcDistance v t = v * t
 
-solve1 :: [RaceDetails] -> Int
-solve1 races = product $ map (length . \(t, d) -> filter (> d) [calcDistance x (t - x) | x <- [0..t]]) races
+solve :: [RaceDetails] -> Int
+solve races = product $ map (length . \(t, d) -> filter (> d) [calcDistance x (t - x) | x <- [0..t]]) races
 
 main :: IO ()
 main = do
-    races <- parse "inputs/input"
-    print $ solve1 races
+    races1 <- parse1 "inputs/input"
+    races2 <- parse2 "inputs/input"
+    print $ solve races1
+    print $ solve [races2]
