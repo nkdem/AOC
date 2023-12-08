ce13c2d8eda4a84e627af152921f7654ea19d293
nkdem <dev@nkdem.net>
2023-12-08 15:28:50 +0000

    Day 3 part b finally completed

--- 2023/day3/3.hs
+++ 2023/day3/3.hs
@@ -1,55 +1,64 @@
-import Data.Functor ((<&>))
-import Data.Char hiding (isSymbol)
-import Data.List (sort, group, isInfixOf, subsequences)
+import Data.Char (isDigit)
+import Data.List (nub, nubBy)
 
-type Grid = (Int, Int)
+getValAt :: (Int, Int) -> [String] -> Char
+getValAt (x, y) grid = (grid !! x) !! y
 
-neighbours :: Grid -> Grid -> [(Int,Int)]
-neighbours pos size = [(x', y') |
-                         x' <- [x, x + 1, x - 1],
-                         x' /= -1,
-                         y' <- [y, y - 1, y + 1],
-                         y' /= -1,
-                         x' /= rows,
-                         y' /= cols,
-                         (x', y') /= (x, y)]
-    where x = fst pos
-          y = snd pos
-          rows = fst size
-          cols = snd size
+-- assumes that the starting position is a digit
+getLeftMostPos :: (Int, Int) -> [String] -> Int
+getLeftMostPos pos@(x, y) xs
+        | y == -1 = 0
+        | not $ isDigit val = y + 1
+        | otherwise = getLeftMostPos (x, y - 1) xs
+    where
+        val = getValAt pos xs
 
-readInput input = readFile input <&> lines
+getRightMostPos :: (Int, Int) -> [String] -> Int
+getRightMostPos pos@(x, y) xs
+        | y == length (head xs) = y
+        | not $ isDigit val = y - 1
+        | otherwise = getRightMostPos (x, y + 1) xs
+    where
+        val = getValAt pos xs
 
-transformDigits :: String -> String -> [Int]
-transformDigits [] acc = concat (replicate (length acc ) [read acc :: Int])
-transformDigits (x:xs) acc
-        | isDigit x = transformDigits xs (acc ++ [x])
-        | isDigit x && not (null acc) = concat (replicate (length acc ) [read acc :: Int]) ++ transformDigits xs []
-        | x == '.' = concat (replicate (length acc ) [read acc :: Int]) ++ -1 : transformDigits xs []
-        | x == '*' = concat (replicate (length acc ) [read acc :: Int]) ++ -2 : transformDigits xs []
-        | otherwise = concat (replicate (length acc ) [read acc :: Int]) ++ -3 : transformDigits xs []
+getDigitsFrom :: (Int, Int) -> [String] -> Int
+getDigitsFrom _ [] = error "grid shouldn't be empty"
+getDigitsFrom pos@(x, y) xs = read (str) :: Int
+    where
+        startIndex = getLeftMostPos pos xs
+        endIndex = getRightMostPos pos xs
+        offset = endIndex - startIndex + 1
+        str = take offset $ drop startIndex (xs !! x)
 
-isSymbol (-2) = True
-isSymbol (-3) = True
-isSymbol _ = False
+type Size = (Int, Int)
+neighbours :: (Int, Int) -> Size -> [(Int, Int)]
+neighbours (x, y) (rows, cols) =
+        [ (x', y')
+        | x' <- [x, x + 1, x - 1]
+        , x' /= -1
+        , y' <- [y, y - 1, y + 1]
+        , y' /= -1
+        , x' /= rows
+        , y' /= cols
+        , (x', y') /= (x, y)
+        ]
 
-solve1 :: [[Int]] -> Grid -> [Int]
-solve1 xs grid = concat [ map head . group  $ [val | (val,y) <- zip row [0..], val /= (-1), any (\(a,b) -> isSymbol $ get a b) (neighbours (x,y) grid)] | (row,x)  <- zip xs [0..] ]
-  where rows = fst grid
-        cols = snd grid
-        get x y = (xs !! x) !! y
+getPairs targetPair list = [(targetPair, x) | x <- list]
 
-elementsExist list1 list2 = all (`elem` list2) list1
-
-solve2 xs grid@(rows,cols) = sum . map (product) . concat . map (map (map head .group . sort) ) $ group $ sort $ filter (\x -> length x >= 2) $ map (\x -> filter (>= 0) $ map (uncurry get ) $ neighbours x grid) filterGears
-  where get x y = (xs !! x) !! y
-        getCordsOf target = filter (\(x,y) -> get x y == target ) [(x,y) | x <- [0..rows - 1], y <- [0..cols -1]]
-        filterGears = filter (\(x,y) -> get x y == -2 ) [(x,y) | x <- [0..rows - 1], y <- [0..cols -1]]
-
+isDigitSame :: (Int, Int) -> (Int, Int) -> [String] -> Bool
+isDigitSame p1@(x, y) p2@(x', y') xs
+        | x == x' = getDigitsFrom p1 xs == getDigitsFrom p2 xs
+        | otherwise = False
 
 main :: IO ()
 main = do
-        input <- readInput "./inputs/input"
-        let gridSize = (length input,length $ head input)
-        print $ sum $ solve1 (map (`transformDigits` []) input) gridSize
-        print $ solve2 (map (`transformDigits` []) input) gridSize
+        contents <- readFile "inputs/input"
+        let xs = lines contents
+            size = (length xs, length $ head xs)
+            grid = concat [[(x, y) | (val, y) <- zip row [0 ..]] | (row, x) <- zip xs [0 ..]]
+            symbols = [pos | pos <- grid, getValAt pos xs /= '.', not $ isDigit $ getValAt pos xs]
+            part1 = concatMap (\symbol -> map (`getDigitsFrom` xs) $ nubBy (\x y -> isDigitSame x y xs) $ filter (\candidate -> isDigit $ getValAt candidate xs) $ neighbours symbol size) symbols
+            gears = [pos | pos <- grid, getValAt pos xs == '*']
+            part2 = map product $ filter (\x -> length x == 2) $ map (\symbol -> map (`getDigitsFrom` xs) $ nubBy (\x y -> isDigitSame x y xs) $ filter (\candidate -> isDigit $ getValAt candidate xs) $ neighbours symbol size) gears
+        print $ sum $ part1
+        print $ sum $ part2
