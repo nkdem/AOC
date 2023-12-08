fee33568645238bc176df23c6f17f6cd8881e396
nkdem <dev@nkdem.net>
2023-12-08 10:14:12 +0000

    Day 8 part b done

--- 2023/day8/8.hs
+++ 2023/day8/8.hs
@@ -29,23 +29,22 @@ constructMap (x:xs) map = constructMap xs (M.insert start (left,right) map)
     where (start:left:right:_) = words $ removeChars ",(=)" x
 
 
-solve1 :: Instructions -> String -> Hashmap -> Counter -> Counter
-solve1 "" _ _ i = i
-solve1 xs key map i
-    | value == "ZZZ" = i + 1
-    | otherwise = solve1 xs value map (i+1)
-        -- trace ("Got " ++ show value ++ " so let's try that!") solve1 xs value map (i+1)
+solve :: Instructions -> String -> Hashmap -> Counter -> Counter
+solve "" _ _ i = i
+solve xs key map i
+    | value !! 2 == 'Z' = i + 1
+    | otherwise = solve xs value map (i+1)
     where instruction = case xs !! (i `mod` length xs) of
             'L' -> fst
             'R' -> snd
             _ -> error "Wrong index dummy"
           value = instruction (fromJust $ M.lookup key map)
 
-
 main :: IO ()
 main = do
     contents <- readFile "inputs/input"
     let (instructions:_:mapUnclean) =  lines contents
-    let map = constructMap mapUnclean M.empty
-    print $ map
-    print $ solve1 instructions "AAA" map 0
+    let hashmap = constructMap mapUnclean M.empty
+    let startingNodes = M.keys $ M.filterWithKey (\k _ -> k !! 2 == 'A' ) hashmap
+    print $ solve instructions "AAA" hashmap 0 -- Part a
+    print $ Prelude.foldl lcm 1 $ Prelude.map (\x -> solve instructions x hashmap 0) startingNodes -- Part b
