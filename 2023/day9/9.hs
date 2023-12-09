e54e2940c4056359a080771113d2db3a7c966afc
nkdem <dev@nkdem.net>
2023-12-09 09:24:20 +0000

    Day 9 part b complete

--- 2023/day9/9.hs
+++ 2023/day9/9.hs
@@ -14,11 +14,14 @@ transform' xs
 extrapolate :: [[[Int]]] -> Int
 extrapolate xs = sum [sum $ map last history | (history, i) <- zip xs [0 ..]]
 
+extrapolate2 xs = sum [foldl (\x y -> y - x) 0 $ reverse $ map head history | (history, i) <- zip xs [0 ..]]
+
 main :: IO ()
 main = do
     xs <- map (map (\x -> read (x) :: Int) . words) . lines <$> readFile "inputs/input"
     let transformedUnclean = map transform' xs
     let transformed = map (\i -> xs !! i : transformedUnclean !! i) [0 .. length xs - 1]
-    print $ extrapolate transformed
+    print $ extrapolate transformed -- part a
+    print $ extrapolate2 $ transformed
 
 -- print xs
