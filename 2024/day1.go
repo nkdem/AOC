ef9675243aa1c105ea6f63ef30fc53e37e43b85a
nkdem <dev@nkdem.net>
2024-12-01 16:37:33 +0000

    Complete Day 1 part 2

--- 2024/day1.go
+++ 2024/day1.go
@@ -34,15 +34,31 @@ func main() {
 		left_list = append(left_list, left)
 		right_list = append(right_list, right)
 	}
-	// sort left list ascending
+	// Part A
 	sort.Ints(left_list)
 	sort.Ints(right_list)
-	fmt.Println(left_list)
-	fmt.Println(right_list)
 
 	total_distance := 0
 	for i := 0; i < len(left_list); i++ {
 		total_distance += distance(left_list[i], right_list[i])
 	}
 	fmt.Println(total_distance)
+
+	// Part B
+	count_map := make(map[int]int)
+	for i := 0; i < len(right_list); i++ {
+		count_map[right_list[i]]++
+	}
+
+	total_simalrity := 0
+	for i := 0; i < len(left_list); i++ {
+		x := left_list[i]
+		// check if x is in the right_list
+		y := 0
+		if count_map[x] > 0 {
+			y = count_map[x]
+		}
+		total_simalrity += (x * y)
+	}
+	fmt.Println(total_simalrity)
 }
