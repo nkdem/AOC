abe876748734ac58d3088d4d59280a1bbb048f7d
nkdem <dev@nkdem.net>
2025-12-01 07:14:00 +0000

    done part 2

--- 2025/day1/day1.go
+++ 2025/day1/day1.go
@@ -19,7 +19,7 @@ func part1(lines []string) int {
 		rotate := element[0]
 		rotateBy := utils.ParseInt(element[1:])
 		if rotate == 'L' {
-			pos = (pos - rotateBy) % RollAt
+			pos = (pos - rotateBy%RollAt + RollAt) % RollAt
 		} else {
 			pos = (pos + rotateBy) % RollAt
 		}
@@ -33,7 +33,33 @@ func part1(lines []string) int {
 }
 
 func part2(lines []string) int {
-	return 0
+	pos := StartingDialPos
+	count := 0
+	for _, element := range lines {
+
+		rotate := element[0]
+		rotateBy := utils.ParseInt(element[1:])
+
+		var newPos int
+		var zeroCrossings int
+
+		if rotate == 'L' {
+			if pos > 0 && rotateBy >= pos {
+				zeroCrossings = (rotateBy-pos)/RollAt + 1
+			} else if pos == 0 && rotateBy > 0 {
+				zeroCrossings = rotateBy / RollAt
+			}
+			newPos = (pos - rotateBy%RollAt + RollAt) % RollAt
+		} else {
+			zeroCrossings = (pos + rotateBy) / RollAt
+			newPos = (pos + rotateBy) % RollAt
+		}
+
+		// fmt.Printf("The dial is rotated %s to point at %d; during this rotation, it points at 0 %d times\n", element, newPos, zeroCrossings)
+		count += zeroCrossings
+		pos = newPos
+	}
+	return count
 }
 
 func main() {
