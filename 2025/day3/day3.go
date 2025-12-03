7ca80d4d87e8d5e0c210a8ee369a0eba9713e50e
nkdem <dev@nkdem.net>
2025-12-03 13:58:00 +0000

    done part 2

--- 2025/day3/day3.go
+++ 2025/day3/day3.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/nkdem/AOC/2025/utils"
 )
@@ -39,8 +40,49 @@ func part1(lines []string) int {
 	return total
 }
 
+const BatteriesRequired = 12
+
+func getHighestValueFromListStartingFrom(values []int, start int, noRequired int) int { // returns index
+	startFrom := len(values) - noRequired
+	max := values[startFrom]
+	max_i := startFrom
+	// fmt.Printf("Starting from %d and the current value is %d (index %d) and after ", startFrom, max, max_i)
+	for i := startFrom - 1; i >= start; i-- {
+		if values[i] >= max {
+			max = values[i]
+			max_i = i
+		}
+
+	}
+	// fmt.Printf("running the loop the highest value was %d (index %d)\n", max, max_i)
+	return max_i
+}
 func part2(lines []string) int {
-	return 0
+	total := 0
+
+	for _, bank := range lines {
+		values := utils.SplitInts(bank, "")
+		indices := []int{}
+		for i := range BatteriesRequired {
+
+			var start int
+			if len(indices) == 0 {
+				start = -1
+			} else {
+				start = indices[len(indices)-1]
+			}
+			index := getHighestValueFromListStartingFrom(values[start+1:], 0, BatteriesRequired-i)
+			index = index + start + 1
+			indices = append(indices, index)
+		}
+		value := 0
+		for i, x := range indices {
+			value += int(math.Pow10(BatteriesRequired-1-i)) * values[x]
+
+		}
+		total += value
+	}
+	return total
 }
 
 func main() {
